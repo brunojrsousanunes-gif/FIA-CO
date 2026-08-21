7a935c9542a05d1917465fa4220207dc7928f643
brunojrsousanunes-gif <brunojrsousanunes@gmail.com>
2026-08-21 07:18:19 +0200

    preprod p5: add technical preproduction gate

---
+++ core/preprod/preprod-gate.mjs
@@ -0,0 +1,27 @@
+export const PREPROD_CHECKS = Object.freeze([
+  'coreOperationContract',
+  'repositoryContract',
+  'organizationIsolation',
+  'auditIntegrity',
+  'recoveryProcedureDefined',
+  'productionExecutionDisabled'
+]);
+
+export function evaluatePreproductionGate(input = {}) {
+  const checks = Object.fromEntries(PREPROD_CHECKS.map(name => [name, input[name] === true]));
+  const missing = PREPROD_CHECKS.filter(name => !checks[name]);
+  const technicalReady = missing.length === 0;
+
+  return Object.freeze({
+    status: technicalReady ? 'PREPROD_TECHNICAL_READY' : 'PREPROD_BLOCKED',
+    technicalReady,
+    missing: Object.freeze([...missing]),
+    checks: Object.freeze(checks),
+    productionExecutionEnabled: false,
+    realFundsEnabled: false,
+    realPiiEnabled: false,
+    externalProviderActivationAllowed: false,
+    explicitFounderAuthorizationRequiredForExternalActivation: true,
+    noAutomaticProductionPromotion: true
+  });
+}
